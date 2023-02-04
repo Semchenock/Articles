66b5b115ba89b1056edcb833622dc664accb1785
SemchenockIP <semchenock@mail.ru>
2023-02-04 23:53:34 +0300

    initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,23 @@
+import webpack from 'webpack';
+import path from 'path';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildPaths, BuildEnv } from './config/build/types/config';
+
+export default function (env: BuildEnv): webpack.Configuration {
+  const paths: BuildPaths = {
+    entery: path.resolve(__dirname, 'src', 'index.tsx'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+  };
+  
+  const mode = env.mode || 'development';
+  const isDev = mode === 'development';
+  const PORT = env.port || 3000;
+  
+  return buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  });
+};
